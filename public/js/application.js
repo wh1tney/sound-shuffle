00932112444a58e1dbc0ae2330eaa2c52f642d30
Whitney O'Banner <wobanner@gmail.com>
2014-08-18 20:00:16 -0700

    Implement track playback

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,7 @@ $(function(){
   // Event handlers
   function bindEvents() {
     $('#connect').on('click', connectCallback);
+    $('#play').on('click', playCallback);
   };
 
   // Event callbacks
@@ -12,6 +13,13 @@ $(function(){
       SC.get('/me', function(user) { 
         alert('Hello, ' + user.username); 
       });
+    });
+  }
+
+  function playCallback(e) {
+    console.log("Play");
+    SC.stream("/tracks/53126096", function(sound){
+      sound.togglePause();
     });
   }
 
