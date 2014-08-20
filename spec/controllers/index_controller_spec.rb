40d494d41d0bf1d044e1309bb7525090e589a9ae
Whitney O'Banner <wobanner@gmail.com>
2014-08-19 17:28:57 -0700

    TDD!!! Write tests for POST /favorites route Add spec_helper file for spec setup

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,3 +1,20 @@
+require 'spec_helper'
+
 describe "SoundCloud API connection" do
-  
+  describe "POST /favorites" do
+    it "responds successfully" do
+      Track.delete_all
+      post '/favorites', {'faves' => {'0' => {track_id:123, title:"Foo", user:"Bar", artwork_url:"www.google.com", waveform_url:"www.google.com"}}}
+      expect(last_response.status).to eq(200)
+    end
+  end
+ 
+  describe "POST /favorites" do
+    it "should create a new Track object" do
+      Track.delete_all
+      expect {
+        post '/favorites', {'faves' => {'0' => {track_id:123, title:"Foo", user:"Bar", artwork_url:"www.google.com", waveform_url:"www.google.com"}}}
+      }.to change(Track, :count)      
+    end
+  end
 end
