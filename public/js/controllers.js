f73db6d14c15b30d7186a829bce2f6792d54f958
Whitney O'Banner <wobanner@gmail.com>
2014-08-21 12:39:05 -0700

    Remove track controller logic from views
    *Cannot get controller logic working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,9 @@
-var plangularControllers = angular.module('plangular'); 
+var plangularController = angular.module('plangular'); 
 
-plangularControllers.controller('tracksController', function($scope) {
-    $scope.tracksList = [
-      {
-          url: "https://soundcloud.com/soulection/tom-misch-the-journey"
-      },
-      {
-          url: "https://soundcloud.com/soulection/tom-misch-the-journey"
-      }
-    ];
+plangularController.controller('tracksController', function($scope) {
+  $scope.tracks = [
+    {
+      url: "https://soundcloud.com/soulection/tom-misch-the-journey"
+    }
+  ]
 });
