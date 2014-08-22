4c0bc01214b053f8d73ecfae08136d68c55ad6ed
Whitney O'Banner <wobanner@gmail.com>
2014-08-21 17:31:28 -0700

    Interpolate sound url from tracks Controller
    *No more hardcoding the track in the view!

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,17 @@
 var plangularController = angular.module('plangular'); 
 
-plangularController.controller('tracksController', function($scope) {
+plangularController.controller('tracksController', function($scope, $sce) {
+
   $scope.tracks = [
     {
-      url: "https://soundcloud.com/soulection/tom-misch-the-journey"
+      src:['https://soundcloud.com/soulection/tom-misch-the-journey']
+    },
+    {
+      src:['https://soundcloud.com/imkuma/elizabeth']
     }
   ]
 });
+
+plangularController.config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?soundcloud\.com/.+$')]);
+}]);
